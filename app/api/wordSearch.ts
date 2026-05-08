55a43cd24714e7be670f3562af230eddc6c3cc11
Anders Norgreen Olsen <andersnorgreen@gmail.com>
2026-05-08 14:08:53 +0200

    Adds the last end to end setup

--- app/api/wordSearch.ts
+++ app/api/wordSearch.ts
@@ -1,11 +1,9 @@
-import { wordSearchRequest, wordSearchResponse } from '@/types/wordSearch.module';
-import { endpoints } from '@/api/endpoints';
-import axiosClient from './axiosClient.ts';
-import axios from 'axios';
+import { wordSearchRequest, wordSearchResponse } from '../types/wordSearch.module';
+import axiosClient from '../utils/axiosClient';
+import { endpoints } from './endpoints';
 
 export const performWordSearch = async (searchData: wordSearchRequest): Promise<wordSearchResponse> => {
     const response = await axiosClient.post<wordSearchResponse>(endpoints.wordSearch.submitSearch, searchData);
-
+    console.log(response)
     return response.data;
   }
-}
--- app/components/WordCountForm.tsx
+++ app/components/WordCountForm.tsx
@@ -1,22 +1,68 @@
 'use client'
 import Form from 'next/form'
+import { useState } from 'react';
+import { findWordsInWebsiteData } from '../utils/wordFinder';
+import { wordSearchRequest } from '../types/wordSearch.module';
+import { performWordSearch } from '../api/wordSearch';
 
 export default function WordCountForm() {
 
-  const handleSubmit = (event: React.FormEvent) => {
+	const [formData, setFormData] = useState({
+		URL: "",
+    searchQuery: ""
+	});
+
+const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const url = document.getElementById("url") as HTMLInputElement;
-    const searchQuery = document.getElementById("searchQuery") as HTMLInputElement;
-    console.log("URL:", url.value, "Query:", searchQuery.value);
-  }
+    const textToSearch = await findWordsInWebsiteData(formData.URL); 
+
+    const wordSearchRequst: wordSearchRequest = {
+      url: formData.URL,
+      searchWord: formData.searchQuery,
+      textToSearch: textToSearch
+    };
+
+    const result = performWordSearch(wordSearchRequst);
+  };
+
+  	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 
   return (
-    <Form action="onSubmit">
-      {/* On submission, the input value will be appended to
+   <div className='flex mx-auto my-auto '>
+      <Form
+        action='  '
+        onSubmit={handleSubmit}
+        className='flex flex-col justify-center items-center gap-4'
+      >
+        {/* On submission, the input value will be appended to
           the URL, e.g. /search?query=abc */}
-      <input name="URL" placeholder='URL' id="url"/>
-      <input name="searchQuery" placeholder='Search word' id="searchQuery"/>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </Form>
+        <input
+          name='URL'
+          placeholder='URL'
+          id='url'
+          className='rounded outline px-4 py-2'
+          onChange={handleChange}
+        />
+        <input
+          name='searchQuery'
+          placeholder='Search word'
+          id='searchQuery'
+          className='rounded outline px-4 py-2'
+          onChange={handleChange}
+        />
+        <button
+          type='submit'
+          className='rounded bg-white text-black px-4 py-1 flex items-center '
+        >
+          Submit
+        </button>
+      </Form>
+    </div>
   )
 }
